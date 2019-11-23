857310eca0949b5ba67218e57afa1e5320e0c994
Tony <bootcamp1955@gmail.com>
2019-11-23 12:08:27 -0500

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const buttons = document.querySelectorAll('button');
+let winner = [0, 0];
 console.log(buttons);
-for(i = 0; i < buttons.length; i++) {
+for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', playGame);
 }
 
 function playGame(e) {
-    console.log(e.target.innerText);
-}
+   let playerSelection = e.target.innerText;
+    let computerSelection = Math.random();
+
+    if (computerSelection < 0.34) {
+        computerSelection = 'Rock';
+    } else if (computerSelection <= 0.67) {
+        computerSelection = 'Paper';
+    } else {
+        computerSelection = 'Scissors';
+    }
+    console.log(playerSelection, computerSelection);
+//     let result = checkWinner(playerSelection, computerSelection);
+//     console.log(result);
+//     if(result == 'Player') {
+//         result += 'wins!';
+//         winner[0]++; 
+//     }else if(result == 'Computer') {
+//         result += 'wins!';
+//         winner[1]++;
+//     }
+//     score.innerHTML = 'Player 1[' +winner[0] + '] Player 2 [' + winner[1] + ']';
+// }
+
+// function checkWinner(pl, co) {
+//     if(pl === co) {
+//         return 'Draw';
+//     }
+//     if(pl === 'Rock') {
+//         if(co === 'Paper') {
+//             return 'Computer';
+//         } else {
+//             return 'Player';
+//         }
+//     }
+//     if(pl === 'Paper') {
+//         if(co === 'Scissors') {
+//             return 'Computer';
+//         } else {
+//             return 'Player';
+//         }
+//     }
+// }
